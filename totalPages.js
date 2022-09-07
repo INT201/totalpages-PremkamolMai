ff6eb62c789d86d58b93dd5581e8c442f70f8b9b
Premkamol Sriyothee <premkamolsriyothee@Macintosh-2.local>
2022-09-08 01:14:24 +0700

    com1

--- totalPages.js
+++ totalPages.js
@@ -1,6 +1,15 @@
 const { template } = require('@babel/core')
+// - กรณี arrayItems เป็น null หรือ undefined ให้ return undefined
+// - กรณี rowsPerPage เป็น null หรือ undefined หรือเป็น 0 จะ return 1
 
 function totalPages(arrayItems, rowsPerPage) {
-  //code here
+  if (arrayItems == null || undefined){
+    return undefined
+  } else if(rowsPerPage === null || rowsPerPage === undefined || rowsPerPage === 0){
+    return 1 
+  } else {
+    let x = arrayItems.length/rowsPerPage
+    return Math.ceil(x)
+  }
 }
 module.exports = totalPages
